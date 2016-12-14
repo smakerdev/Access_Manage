5ddd6956394eba0f2c90f0514432db3c447fcb9c
Smaker <admin@smaker.kr>
2016-12-14 20:24:46 +0900

    Clear Project....
    Because of error

--- src/main/java/kr/smaker/access/main/HomeController.java
+++ src/main/java/kr/smaker/access/main/HomeController.java
@@ -1,4 +1,4 @@
-package kr.smaker.access.Controller;
+package kr.smaker.access.main;
 
 import java.text.DateFormat;
 import java.util.Date;
---
+++ src/main/java/kr/smaker/access/member/UserVO.java
@@ -0,0 +1,40 @@
+package kr.smaker.access.member;
+
+public class UserVO {
+	private String userno;
+	private String userid;
+	private String userpw;
+	private String userrole;
+
+	public String getUserno() {
+		return userno;
+	}
+
+	public void setUserno(String userno) {
+		this.userno = userno;
+	}
+
+	public String getUserid() {
+		return userid;
+	}
+
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+
+	public String getUserpw() {
+		return userpw;
+	}
+
+	public void setUserpw(String userpw) {
+		this.userpw = userpw;
+	}
+
+	public String getUserrole() {
+		return userrole;
+	}
+
+	public void setUserrole(String userrole) {
+		this.userrole = userrole;
+	}
+}
