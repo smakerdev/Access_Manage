9bfaee8a1af54530c50afdb5fc5ca27974b5eed1
Choi Ji Hyun <admin@smaker.kr>
2016-12-18 01:47:08 +0900

    Change Somethings

--- src/main/java/kr/smaker/access/main/RequestOutCtr.java
+++ src/main/java/kr/smaker/access/main/RequestOutCtr.java
@@ -1,12 +1,21 @@
 package kr.smaker.access.main;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class RequestOutCtr {
-	@RequestMapping(value = "/RequestOut")
-	public String requestOut() {
+	@RequestMapping(value = "/requestOut")
+	public String requestOut(HttpServletRequest request) {
+		String userno = request.getSession().getAttribute("userno").toString();
+		String usernm = request.getSession().getAttribute("usernm").toString();
+		String userrole = request.getSession().getAttribute("userrole").toString();
+		
+		if (userrole.equals("A")) {
+			return "main/index";
+		}
 		return "main/RequestOut";
 	}
 }
